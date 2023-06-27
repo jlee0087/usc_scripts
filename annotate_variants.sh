aa85612b66a873be6cf27d486586738d93f8c14f
John Lee USC <jlee0087@usc.edu>
2023-06-27 10:35:30 -0700

    made changes to java command  of variant annotation

--- annotate_variants.sh
+++ annotate_variants.sh
@@ -10,17 +10,17 @@
 ###########################
 # SETUP
 ###########################
-module load gcc/8.3.0 snpeff
+module load gcc/8.3.0 snpeff openjdk
 
 ###########################
 # VARIABLES
 ###########################
 workingDir="/scratch1/jlee0087/projects/exome_exercise/vcfs"
 bamDir="/scratch1/jlee0087/projects/exome_exercise/bams"
-
+snpeffpath="/project/davidwcr_264/Packages/snpEff_new/4.3/"
 ###########################
 # COMMANDS
 ###########################
 cd $workingDir
-snpEff eff GRCh37.75 $workingDir/NA12878.freebayes.vcf > $workingDir/NA12878.freebayes.snpeff.vcf
+java -Xmx16g -jar $snpeffpath/snpEff.jar GRCh37.75 $workingDir/NA12878.freebayes.vcf > $workingDir/NA12878.freebayes.snpeff.vcf
 
